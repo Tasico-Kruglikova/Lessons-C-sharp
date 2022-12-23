5044edd1c2835346f36c1e89bfbf4e250ea37948
Tasico Kruglikova <prosto-lo@yandex.ru>
2022-12-23 15:33:23 +0500

    Changed Example004_Div

--- Lesson1/Example004_Div/Program.cs
+++ Lesson1/Example004_Div/Program.cs
@@ -1,3 +1,9 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+double numberA = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
